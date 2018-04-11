b2f4e76aa8dad851eea5516d403e79bc63a5634b
kimdonwon <ehddnjs0728@naver.com>
2018-04-11 17:28:30 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/apdlz/test/MainActivity.java
+++ app/src/main/java/com/example/apdlz/test/MainActivity.java
@@ -22,7 +22,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, SecondLayout.OML{
-    //mirrortest
+    //mirrortest123
     static String ipset;
 
     @Override
